3ea2921a4153c5bbce86b8b4d8ec49ada7355662
Abhishek Bhowmick <abhowmick22@gmail.com>
2023-08-12 07:09:06 -0700

    Finish intro to langchain

--- intro.py
+++ intro.py
@@ -1,4 +1,5 @@
 from langchain import PromptTemplate, HuggingFaceHub, LLMChain
+from langchain.llms import OpenAI
 import os
 
 template = """Question: {question}
@@ -9,22 +10,56 @@ prompt = PromptTemplate(
     input_variables=['question']
 )
 
-# initialize Hub LLM
-os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
+# initialize Hub LLM (doesn't work)
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = 'HUGGINGFACE_KEY' # https://huggingface.co/settings/tokens
 hub_llm = HuggingFaceHub(
         repo_id='google/flan-t5-xl',
     model_kwargs={'temperature':1e-10}
 )
 
-print("here")
+# initialize OpenAI
+# os.environ['OPENAI_API_KEY'] = 'OPENAI_KEY'
+davinci = OpenAI(model_name='text-davinci-003')
+
+# user questions, asking one at a time
+question = "Which NFL team won the Super Bowl in the 2010 season?"
+questions = [
+    {'question': "Which NFL team won the Super Bowl in the 2010 season?"},
+    {'question': "If I am 6 ft 4 inches, how tall am I in centimeters?"},
+    {'question': "Who was the 12th person on the moon?"},
+    {'question': "How many eyes does a blade of grass have?"}
+]
+
 # create prompt template > LLM chain
 llm_chain = LLMChain(
     prompt=prompt,
-    llm=hub_llm
+    llm=davinci
 )
 
-# user question
-question = "Which NFL team won the Super Bowl in the 2010 season?"
+print(llm_chain.generate(questions))
 
-# ask the user question about NFL 2010
-print(llm_chain.run(question))
+# user questions, ask all in one prompt
+multi_template = """Answer the following questions one at a time.
+
+Questions:
+{questions}
+
+Answers:
+"""
+
+long_prompt = PromptTemplate(template=multi_template, input_variables=["questions"])
+
+qs_str = (
+    "Which NFL team won the Super Bowl in the 2010 season?\n" +
+    "If I am 6 ft 4 inches, how tall am I in centimeters?\n" +
+    "Who was the 12th person on the moon?\n" +
+    "How many eyes does a blade of grass have?"
+)
+
+# create prompt template > LLM chain
+llm_chain = LLMChain(
+    prompt=long_prompt,
+    llm=davinci
+)
+
+print(llm_chain.run(qs_str))
